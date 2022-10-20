66dab2117f46071ab4a36df4979c2f038fa17758
Лиза Чабан <komissarovkaliza@gmail.com>
2022-10-20 20:18:09 +0300

    feat: add CurrencyService

---
+++ ServiceTests/CurrencyServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Models;
+using Services;
+using Xunit;
+
+namespace ServiceTests
+{
+    public class CurrencyServiceTests
+    {
+        [Fact]
+        public async Task RequestTest()
+        {
+            //Arrange
+            var service = new CurrencyService();
+            var account = new Account() {AccountId = Guid.NewGuid(), Amount = 500, Currency = new Currency() { Name = "USD"} };
+            var currencyToConvert = "RUB";
+            var apiKey = "QfGyJd6V7rbnaLkjgV7VY9rx5TsPUr";
+
+            //Act
+            var newAmount = await service.CurrencyConversationAsync(account, currencyToConvert, apiKey);
+
+            //Assert
+            Assert.NotEqual(0, newAmount);
+         
+        }
+    }
+}
---
+++ Services/CurrencyService.cs
@@ -0,0 +1,20 @@
+﻿using Models;
+using Newtonsoft.Json;
+
+namespace Services
+{
+    public class CurrencyService
+    {
+        public async Task<decimal> CurrencyConversationAsync(Account account, string currencyTo, string apiKey)
+        {
+                 
+            var uriString = $"https://www.amdoren.com/api/currency.php?api_key={apiKey}&from={account.Currency.Name}&to={currencyTo}&amount={account.Amount}";
+            var endpoit = new Uri(uriString);
+
+            using var client = new HttpClient();
+            var amountResponse = JsonConvert.DeserializeObject<AmountResponce>(await client.GetAsync(endpoit).Result.Content.ReadAsStringAsync());
+
+            return amountResponse.amount;
+        }
+    }
+}
